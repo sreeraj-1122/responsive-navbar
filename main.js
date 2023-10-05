9589c07ffb06f22d1e7d53aff7a96736cc1fc175
sreeraj <sreeraj2122@gmail.com>
2023-10-05 15:12:52 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.menu');
+let icon = document.querySelector('.menu-icon');
+
+icon.addEventListener('click',function(){
+
+    menu.classList.toggle('is-active')
+
+});
